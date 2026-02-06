31e9aa25574bee5faab4567d0969362aa2ab30d5
André Alves <andre.alves2006@hotmail.com>
2026-02-06 12:59:00 -0300

    feat: add Feature componente and color pallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
-import styles from "./page.module.css";
+import Feature from "@/app/components/feature/feature";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
-    <></>
+    <div>
+      <section>
+        <div className={styles.featureContainer}>
+          <Feature
+            title="Feature title"
+            iconSrc="/speed.svg"
+          >
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum id reprehenderit magni quia rem porro deserunt aut maxime distinctio ab perspiciatis rerum ullam, placeat, quos voluptatibus repellat neque eos perferendis.
+            </p>
+          </Feature>
+          
+          <Feature
+            title="Feature title"
+            iconSrc="/speed.svg"
+          >
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum id reprehenderit magni quia rem porro deserunt aut maxime distinctio ab perspiciatis rerum ullam, placeat, quos voluptatibus repellat neque eos perferendis.
+            </p>
+          </Feature>
+          
+          <Feature
+            title="Feature title"
+            iconSrc="/speed.svg"
+          >
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum id reprehenderit magni quia rem porro deserunt aut maxime distinctio ab perspiciatis rerum ullam, placeat, quos voluptatibus repellat neque eos perferendis.
+            </p>
+          </Feature>
+        </div>
+      </section>
+    </div>
   );
 }
