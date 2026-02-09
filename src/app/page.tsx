2a1b068366ccce28f094c02750dfc26eb2d0e24c
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2026-02-09 15:05:23 -0300

    chore(page.tsx): remove unused imports for Card and Icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import {Card} from "./components/card/card.tsx";
-import {Icon} from "@iconify/react";
 
 export default function Home() {
   return (
